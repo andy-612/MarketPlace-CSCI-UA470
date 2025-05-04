ad600e583181cbc47974d4837ebd09d31c310f6c
andy-612 <liubai.andy@gmail.com>
2025-05-04 10:35:35 -0400

    Center windows

--- src/gui/ModifyProductsGUI.java
+++ src/gui/ModifyProductsGUI.java
@@ -21,26 +21,29 @@ public class ModifyProductsGUI extends JFrame {
         JPanel contentPane = new JPanel(null);
         setContentPane(contentPane);
 
-        String[] cols = {"Product Name","Price","Quantity"};
-        model = new DefaultTableModel(fetchData(), cols){
+        String[] cols = { "Product Name", "Price", "Quantity" };
+        model = new DefaultTableModel(fetchData(), cols) {
             @Override
-            public boolean isCellEditable(int row, int column){
+            public boolean isCellEditable(int row, int column) {
                 return false;
             }
         };
         table = new JTable(model);
         JScrollPane scroll = new JScrollPane(table);
-        scroll.setBounds(20,20,460,220);
+        scroll.setBounds(20, 20, 460, 220);
         contentPane.add(scroll);
 
-        txtNewName = new JTextField(); txtNewName.setBounds(130,260,200,25);
-        contentPane.add(new JLabel("New Name:")).setBounds(20,260,100,25);
+        txtNewName = new JTextField();
+        txtNewName.setBounds(130, 260, 200, 25);
+        contentPane.add(new JLabel("New Name:")).setBounds(20, 260, 100, 25);
         contentPane.add(txtNewName);
-        txtNewPrice = new JTextField(); txtNewPrice.setBounds(130,300,200,25);
-        contentPane.add(new JLabel("New Price:")).setBounds(20,300,100,25);
+        txtNewPrice = new JTextField();
+        txtNewPrice.setBounds(130, 300, 200, 25);
+        contentPane.add(new JLabel("New Price:")).setBounds(20, 300, 100, 25);
         contentPane.add(txtNewPrice);
-        txtNewQuantity = new JTextField(); txtNewQuantity.setBounds(130,340,200,25);
-        contentPane.add(new JLabel("New Quantity:")).setBounds(20,340,100,25);
+        txtNewQuantity = new JTextField();
+        txtNewQuantity.setBounds(130, 340, 200, 25);
+        contentPane.add(new JLabel("New Quantity:")).setBounds(20, 340, 100, 25);
         contentPane.add(txtNewQuantity);
 
         table.getSelectionModel().addListSelectionListener(e -> {
@@ -59,7 +62,7 @@ public class ModifyProductsGUI extends JFrame {
         });
 
         JButton btnUpdate = new JButton("Update");
-        btnUpdate.setBounds(360,260,120,30);
+        btnUpdate.setBounds(360, 260, 120, 30);
         btnUpdate.addActionListener(e -> {
             int r = table.getSelectedRow();
             Product prod = productManager.getProducts().get(r);
@@ -67,45 +70,45 @@ public class ModifyProductsGUI extends JFrame {
             prod.setPrice(Double.parseDouble(txtNewPrice.getText()));
             prod.setQuantity(Integer.parseInt(txtNewQuantity.getText()));
             DataUtil.saveProducts(productManager.getProducts());
-            model.setValueAt(prod.getName(),r,0);
-            model.setValueAt(prod.getPrice(),r,1);
-            model.setValueAt(prod.getQuantity(),r,2);
-            JOptionPane.showMessageDialog(this,"Product updated successfully!");
+            model.setValueAt(prod.getName(), r, 0);
+            model.setValueAt(prod.getPrice(), r, 1);
+            model.setValueAt(prod.getQuantity(), r, 2);
+            JOptionPane.showMessageDialog(this, "Product updated successfully!");
         });
         contentPane.add(btnUpdate);
 
         JButton btnDelete = new JButton("Delete");
-        btnDelete.setBounds(360,310,120,30);
+        btnDelete.setBounds(360, 310, 120, 30);
         btnDelete.addActionListener(e -> {
             int r = table.getSelectedRow();
             if (r == -1) {
                 JOptionPane.showMessageDialog(this, "Please select a product to delete.");
                 return;
             }
-        
+
             Product prod = productManager.getProducts().get(r);
             productManager.getProducts().remove(prod);
-            DataUtil.saveProducts(productManager.getProducts());        
+            DataUtil.saveProducts(productManager.getProducts());
 
             model.removeRow(r);
 
             txtNewName.setText("");
             txtNewPrice.setText("");
             txtNewQuantity.setText("");
-        
+
             JOptionPane.showMessageDialog(this, "Product deleted successfully!");
         });
         contentPane.add(btnDelete);
-
+        setLocationRelativeTo(null);
         setVisible(true);
     }
 
-    private Object[][] fetchData(){
+    private Object[][] fetchData() {
         List<Product> list = productManager.getProducts();
         Object[][] arr = new Object[list.size()][3];
-        for(int i=0;i<list.size();i++){
+        for (int i = 0; i < list.size(); i++) {
             Product p = list.get(i);
-            arr[i] = new Object[]{p.getName(),p.getPrice(),p.getQuantity()};
+            arr[i] = new Object[] { p.getName(), p.getPrice(), p.getQuantity() };
         }
         return arr;
     }
--- src/gui/UploadProductsGUI.java
+++ src/gui/UploadProductsGUI.java
@@ -15,33 +15,39 @@ public class UploadProductsGUI extends JFrame {
         JPanel contentPane = new JPanel(null);
         setContentPane(contentPane);
 
-        JLabel lblName = new JLabel("Name:"); lblName.setBounds(20, 20, 100, 25);
+        JLabel lblName = new JLabel("Name:");
+        lblName.setBounds(20, 20, 100, 25);
         contentPane.add(lblName);
-        txtName = new JTextField(); txtName.setBounds(120, 20, 220, 25);
+        txtName = new JTextField();
+        txtName.setBounds(120, 20, 220, 25);
         contentPane.add(txtName);
 
-        JLabel lblPrice = new JLabel("Price:"); lblPrice.setBounds(20, 60, 100, 25);
+        JLabel lblPrice = new JLabel("Price:");
+        lblPrice.setBounds(20, 60, 100, 25);
         contentPane.add(lblPrice);
-        txtPrice = new JTextField(); txtPrice.setBounds(120, 60, 220, 25);
+        txtPrice = new JTextField();
+        txtPrice.setBounds(120, 60, 220, 25);
         contentPane.add(txtPrice);
 
-        JLabel lblQty = new JLabel("Quantity:"); lblQty.setBounds(20, 100, 100, 25);
+        JLabel lblQty = new JLabel("Quantity:");
+        lblQty.setBounds(20, 100, 100, 25);
         contentPane.add(lblQty);
-        txtQuantity = new JTextField(); txtQuantity.setBounds(120, 100, 220, 25);
+        txtQuantity = new JTextField();
+        txtQuantity.setBounds(120, 100, 220, 25);
         contentPane.add(txtQuantity);
 
         JButton btnUpload = new JButton("Upload Item");
         btnUpload.setBounds(140, 160, 120, 30);
         btnUpload.addActionListener(e -> performUpload());
         contentPane.add(btnUpload);
-
+        setLocationRelativeTo(null);
         setVisible(true);
     }
 
     private void performUpload() {
-        String name  = txtName.getText();
+        String name = txtName.getText();
         double price = Double.parseDouble(txtPrice.getText());
-        int qty       = Integer.parseInt(txtQuantity.getText());
+        int qty = Integer.parseInt(txtQuantity.getText());
         productManager.addProduct(name, price, qty);
         JOptionPane.showMessageDialog(this, "Product added!");
         dispose();
