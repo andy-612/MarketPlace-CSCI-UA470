28f0e82a2a156e0a02720d2a2c3c13fcdcedaf93
andy-612 <liubai.andy@gmail.com>
2025-05-04 12:21:06 -0400

    MarketPlace Function Updates

--- src/gui/MarketPlaceGUI.java
+++ src/gui/MarketPlaceGUI.java
@@ -18,7 +18,7 @@ public class MarketPlaceGUI extends JFrame {
 
     public MarketPlaceGUI(String username, ProfileManager pm, ProductManager pr) {
         super("Marketplace - Welcome, " + username);
-        this.username       = username;
+        this.username = username;
         this.profileManager = pm;
         this.productManager = pr;
 
@@ -38,59 +38,39 @@ public class MarketPlaceGUI extends JFrame {
 
     private void buildUI() {
         String[] columns = { "Name", "Price", "Quantity", "Review" };
-        DefaultTableModel tableModel = new DefaultTableModel(columns, 0){
+        DefaultTableModel tableModel = new DefaultTableModel(columns, 0) {
             @Override
-            public boolean isCellEditable(int row, int column){
+            public boolean isCellEditable(int row, int column) {
                 return false;
             }
         };
         productTable = new JTable(tableModel);
         updateTable(tableModel);
 
-        JButton buyButton    = new JButton("Buy");
+        JButton buyButton = new JButton("Buy");
         JButton returnButton = new JButton("Return");
         JButton reviewButton = new JButton("Leave Review");
         JButton modifyButton = new JButton("Modify Profile");
-        JButton logoutButton = new JButton("Logout to Main Menu");
+        JButton logoutButton = new JButton("Logout");
 
         buyButton.addActionListener(e -> {
-            int r = productTable.getSelectedRow();
-            if (r >= 0) {
-                String name = productTable.getValueAt(r, 0).toString();
-                if (productManager.reduceQuantity(name)) {
-                    profileManager.addPurchaseToProfile(username, new Product(name,
-                        Double.parseDouble(productTable.getValueAt(r,1).toString()), 1));
-                    JOptionPane.showMessageDialog(this, "Purchase successful.");
-                    updateTable((DefaultTableModel)productTable.getModel());
-                }
-            }
+            onBuyClick();
         });
 
         returnButton.addActionListener(e -> {
-            int r = productTable.getSelectedRow();
-            if (r >= 0) {
-                String name = productTable.getValueAt(r, 0).toString();
-                if (profileManager.removePurchaseFromProfile(username, new Product(name,0,1))) {
-                    productManager.increaseQuantity(name);
-                    JOptionPane.showMessageDialog(this, "Return successful.");
-                    updateTable((DefaultTableModel)productTable.getModel());
-                }
-            }
+            onReturnClick();
         });
 
         reviewButton.addActionListener(e -> {
-            new ReviewGUI(username, productManager, profileManager);
-            dispose();
+            onReviewClick();
         });
 
         modifyButton.addActionListener(e -> {
-            new MyProfileGUI(username, profileManager, productManager);
-            dispose();
+            onModifyProfileClick();
         });
 
         logoutButton.addActionListener(e -> {
-            new MainGUI(profileManager, productManager);
-            dispose();
+            onLogoutClick();
         });
 
         JPanel buttonPanel = new JPanel(new GridLayout(1, 5, 10, 0));
@@ -100,7 +80,7 @@ public class MarketPlaceGUI extends JFrame {
         buttonPanel.add(modifyButton);
         buttonPanel.add(logoutButton);
 
-        setLayout(new BorderLayout(10,10));
+        setLayout(new BorderLayout(10, 10));
         add(new JScrollPane(productTable), BorderLayout.CENTER);
         add(buttonPanel, BorderLayout.SOUTH);
     }
@@ -108,12 +88,53 @@ public class MarketPlaceGUI extends JFrame {
     private void updateTable(DefaultTableModel model) {
         model.setRowCount(0);
         for (Product p : productManager.getProducts()) {
-            model.addRow(new Object[]{
-                p.getName(),
-                p.getPrice(),
-                p.getQuantity(),
-                p.getLatestReview()
+            model.addRow(new Object[] {
+                    p.getName(),
+                    p.getPrice(),
+                    p.getQuantity(),
+                    p.getLatestReview()
             });
         }
     }
+
+    public void onBuyClick() {
+        int r = productTable.getSelectedRow();
+        if (r >= 0) {
+            String name = productTable.getValueAt(r, 0).toString();
+            if (productManager.reduceQuantity(name)) {
+                profileManager.addPurchaseToProfile(username, new Product(name,
+                        Double.parseDouble(productTable.getValueAt(r, 1).toString()), 1));
+                JOptionPane.showMessageDialog(this, "Purchase successful.");
+                updateTable((DefaultTableModel) productTable.getModel());
+            }
+        }
+    }
+
+    public void onReturnClick() {
+        int r = productTable.getSelectedRow();
+        if (r >= 0) {
+            String name = productTable.getValueAt(r, 0).toString();
+            if (profileManager.removePurchaseFromProfile(username, new Product(name, 0, 1))) {
+                productManager.increaseQuantity(name);
+                JOptionPane.showMessageDialog(this, "Return successful.");
+                updateTable((DefaultTableModel) productTable.getModel());
+            }
+        }
+    }
+
+    public void onReviewClick() {
+        new ReviewGUI(username, productManager, profileManager);
+        dispose();
+    }
+
+    public void onModifyProfileClick() {
+        new MyProfileGUI(username, profileManager, productManager);
+        dispose();
+    }
+
+    public void onLogoutClick() {
+        new MainGUI(profileManager, productManager);
+        dispose();
+    }
+
 }
